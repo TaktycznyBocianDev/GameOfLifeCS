e22f33717213440a5c888b1773102ae0691e3d2f
Stanisław Franczak <s.franczak20@gmail.com>
2025-04-21 14:32:54 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        const int screenWidth = 800;
+        const int screenHeight = 600;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "Raylib-CS: Starter Template");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            // LOGIKA GRY TU
+            Update();
+
+            // RYSOWANIE TU
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DarkGray);
+
+            Draw();
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    static void Update()
+    {
+        // np. porusz obiekt
+    }
+
+    static void Draw()
+    {
+        Raylib.DrawText("Hello Raylib-CS!", 10, 10, 20, Color.RayWhite);
+    }
+}
