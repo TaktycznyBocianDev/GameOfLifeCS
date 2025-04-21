4542e8480af5a2733fb3e239836b87dbb488593b
Stanisław Franczak <s.franczak20@gmail.com>
2025-04-21 23:51:43 +0200

    Adding comments, polishing, finishing

--- Grid.cs
+++ Grid.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Raylib_cs;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Raylib_cs;
 
 namespace GameOfLifeCS
 {
+    /// <summary>
+    /// Represents the cell grid in the game of life.
+    /// Allows to initialize, draw, evaluate and change states of cells.
+    /// </summary>
     public class World
     {
 
@@ -15,13 +13,22 @@ namespace GameOfLifeCS
         int GridSize { get; set;}
         int CellSize { get; set;}
 
+        private readonly int randomChance;
+        private readonly Random random = new();
+
         public enum State
         {
             Dead,
             Alive
         }
 
-        public World(int GridSize, int CellSize) 
+        /// <summary>
+        /// Initialize new World → a 2-dimensional grid of States (Dead and Alive). It will fill the grid with dead cells and a random amount of alive cells.
+        /// </summary>
+        /// <param name="GridSize">Size of the grid → [size, size].</param>
+        /// <param name="CellSize">Size of the one cell in pixels.</param>
+        /// <param name="randomChance">Integer between 0 and 100. The bigger, then bigger the chance for alive cells. Will change values lower than 0 and bigger than 100 to 100.</param>
+        public World(int GridSize, int CellSize, int randomChance) 
         {
         
             //Setting new grid
@@ -29,16 +36,20 @@ namespace GameOfLifeCS
             this.GridSize = GridSize;
             this.CellSize = CellSize;
 
+            if (randomChance > 100 || randomChance < 0 ) randomChance = 100;
+            this.randomChance = randomChance;
+            
             Grid = SetAllDead(Grid);
             Grid = SetRandAlive(Grid);
 
         }
 
         /// <summary>
-        /// Sets all cells dead (as metal as it sounds!).
+        /// Takes any grid, sets all cells dead (as metal as it sounds!) and returns this grid.
         /// </summary>
-        /// <param name="size"></param>
-        public State[,] SetAllDead(State[,] grid) 
+        /// <param name="grid">The grid to change.</param>
+        /// <returns>Grid with all cells set as dead.</returns>
+        private State[,] SetAllDead(State[,] grid) 
         {
             for (int i = 0; i < GridSize; i++)
             {
@@ -48,14 +59,18 @@ namespace GameOfLifeCS
             return grid;
         }
 
-        Random random = new Random();
-        public State[,] SetRandAlive(State[,] grid)
+        /// <summary>
+        /// Takes any grid, sets random amount of them as alive, and returns this grid.
+        /// </summary>
+        /// <param name="grid">Grid to change</param>
+        /// <returns>Grid with some of the cells set as alive.</returns>
+        private State[,] SetRandAlive(State[,] grid)
         {
             for (int i = 0; i < GridSize; i++)
             {
                 for (int j = 0; j < GridSize; j++)
 
-                    if (random.Next(100) < 10)
+                    if (random.Next(100) < randomChance)
                     {
                         grid[i, j] = State.Alive;
                     }
@@ -64,7 +79,7 @@ namespace GameOfLifeCS
         }
 
         /// <summary>
-        /// Draws Grid with slight borders around avery cell. Uses "switch" to go for every posible cell state.
+        /// Draws Grid with slight borders around every cell. Uses "switch" to go for every possible cell state.
         /// </summary>
         public void DrawGrid()
         {
@@ -94,23 +109,25 @@ namespace GameOfLifeCS
             }
         }
         /// <summary>
-        /// Allows for simple change in the cell state → mostly for Mouse Controll
+        /// Allows for simple change in the cell state → mostly for Mouse Control
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
+        /// <param name="x">The x coordinate for the cell.</param>
+        /// <param name="y">The y coordinate for the cell.</param>
         public void ChangeStateSimple(int x, int y)
         {
             if (Grid[x,y] == State.Dead) Grid[x,y] = State.Alive;
             else if (Grid[x,y] == State.Alive) Grid[x,y] = State.Dead;
         }
-
+        /// <summary>
+        /// The most important function of this class. It creates a copy of the current grid and creates a new one; then performs calculations and sets the new grid as the Grid with updated states of the cells. Skips edge cases because of yes (I don't want to do them).
+        /// </summary>
         public void GameOfLife()
         {
             //Copy new grid and crate new one → that will replace the old one after calculating changes
             State[,] currentGrid = (State[,])Grid.Clone();
             State[,] newGrid = SetAllDead(new State[GridSize, GridSize]);
 
-            //Go to every cell and calculate if it need to be changed. Leave edge cases as i dont want to.
+            //Go to every cell and calculate if it need to be changed. Leave edge cases as i don't want to do it.
             for (int i = 1; i < Grid.GetLength(0) - 1; i++)
             {
                 for (int j = 1; j < Grid.GetLength(1) - 1; j++)
@@ -140,15 +157,14 @@ namespace GameOfLifeCS
             Grid = newGrid;
 
         }
-
         /// <summary>
-        /// Find Moore Neighbours for cell and count alive and dead cells in it. 
+        /// Find Moore Neighbors for a cell and count alive and dead cells in it. 
         /// </summary>
-        /// <param name="currentGrid"></param>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <returns></returns>
-        private (int live, int dead) CountNeighbours(State[,] currentGrid, int x, int y)
+        /// <param name="currentGrid">The grid that is used for the calculations.</param>
+        /// <param name="x">The X coordinate of the cell.</param>
+        /// <param name="y">The Y coordinate of the cell.</param>
+        /// <returns>Tuple of live and dead cells (live, dead)</returns>
+        private static (int live, int dead) CountNeighbours(State[,] currentGrid, int x, int y)
         {
             int live = 0;
             int dead = 0;
--- Program.cs
+++ Program.cs
@@ -3,64 +3,64 @@ using Raylib_cs;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 
-class Program
+namespace GameOfLifeCS
 {
-    static void Main()
-    { 
-        const int screenSize = 800;
-
-        int gridSize = 800;
-        int cellSize = screenSize/gridSize;
-
-        World world = new World(gridSize, cellSize);
-
-        Raylib.InitWindow(screenSize, screenSize, "Game Of Life");
-        Raylib.SetTargetFPS(20);
-
-        bool paused = true;
-
-        while (!Raylib.WindowShouldClose())
+    class Program
+    {
+        static void Main()
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.DarkGray);
+            const int screenSize = 1000; 
+            const int gridSize = 250;
+            const int cellSize = screenSize / gridSize;
+            const int randomChance = 15; //ONLY from 0 to 100 (Will be changed to 100 if outside this clamps).
+            World world = new World(gridSize, cellSize, randomChance);
+            bool paused = true;
 
-            if (Raylib.IsKeyPressed(KeyboardKey.Space))
-                paused = !paused;
+            Raylib.InitWindow(screenSize, screenSize, "Game Of Life");
+            Raylib.SetTargetFPS(60);
 
 
-            
-            if (paused) MouseClickOnGrid(world, cellSize);
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DarkGray);
 
-            world.DrawGrid();
-            Raylib.DrawText(paused ? "PAUSED (SPACE to run)" : "RUNNING (SPACE to pause)", 10, 10, 20, Color.RayWhite);
-            
-            if (!paused) world.GameOfLife();
-           
-            Raylib.EndDrawing();
+                if (Raylib.IsKeyPressed(KeyboardKey.Space))
+                    paused = !paused;
+
+                if (paused) MouseClickOnGrid(world, cellSize);
+
+                world.DrawGrid();
+
+                Raylib.DrawText(paused ? "PAUSED (SPACE to run)" : "RUNNING (SPACE to pause)", 10, 10, 20, Color.RayWhite);
+
+                if (!paused) world.GameOfLife();
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
 
-        Raylib.CloseWindow();
-    }
-
-    public static void MouseClickOnGrid(World world, int cellSize)
-    {
-        if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+        /// <summary>
+        /// Allows changing State of the cell by clicking on it. 
+        /// </summary>
+        /// <param name="world">Current world of the game of life.</param>
+        /// <param name="cellSize">It is necessary to convert pixel coordinates to grid coordinates.</param>
+        public static void MouseClickOnGrid(World world, int cellSize)
         {
-            Vector2 mousePos = Raylib.GetMousePosition();
+            if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+            {
+                Vector2 mousePos = Raylib.GetMousePosition();
 
-            int x = (int)(mousePos.X / cellSize);
-            int y = (int)(mousePos.Y / cellSize);
+                int x = (int)(mousePos.X / cellSize);
+                int y = (int)(mousePos.Y / cellSize);
 
-            world.ChangeStateSimple(x, y);
+                world.ChangeStateSimple(x, y);
 
-            Console.WriteLine("Click: " + x + ", " + y);
+                Console.WriteLine("Click: " + x + ", " + y);
 
+            }
         }
-    }
-
-
-    static void Draw()
-    {
-        Raylib.DrawText("Hello Raylib-CS!", 10, 10, 20, Color.RayWhite);
     }
 }
